89641f528600f86160e125e51a717b25d8378374
parth <parthkanani8490@gmail.com>
2020-04-07 12:47:27 -0700

    Updates

---
+++ precache-manifest.83d5cf6f25d0386a1449ba78791da253.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f2f425dd99f1ece44876b88589948730",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "19e6ca1e51ceb17db814",
+    "url": "/monster-rolodex/static/css/main.1b5e9782.chunk.css"
+  },
+  {
+    "revision": "1bb5dd2a6657ff2ee685",
+    "url": "/monster-rolodex/static/js/2.b6f2604e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monster-rolodex/static/js/2.b6f2604e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "19e6ca1e51ceb17db814",
+    "url": "/monster-rolodex/static/js/main.6828176a.chunk.js"
+  },
+  {
+    "revision": "90506cc279e62bb14608",
+    "url": "/monster-rolodex/static/js/runtime-main.59621b72.js"
+  }
+]);
